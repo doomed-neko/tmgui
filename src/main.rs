1125665b4a6a776d6180c043ae94984900d7461f
Adenosine <79870712+doomed-neko@users.noreply.github.com>
2025-07-28 01:05:34 +0300

    add "load more" button for better pagination + ui fixes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use std::sync::{Arc, Mutex};
 
-use chrono::TimeZone;
+use chrono::{Datelike, Month, TimeZone, Timelike};
 use eframe::{
     App,
     egui::{
-        self, CentralPanel, ComboBox, Context, FontId, Frame, ScrollArea, Separator, Spinner,
-        Stroke, TextStyle, TopBottomPanel, ViewportBuilder, Widget,
+        self, CentralPanel, ComboBox, Context, FontId, Frame, Margin, RichText, ScrollArea,
+        Separator, Spinner, Stroke, TextStyle, TopBottomPanel, ViewportBuilder, Widget,
     },
 };
 use futures::executor::block_on;
 use rand::Rng;
 use tmapi::{Client, Email};
+
+const EMAIL_FETCHING_LIMIT: u8 = 50;
 
 struct TmApp {
     domain: String,
@@ -18,7 +20,9 @@ struct TmApp {
     name: String,
     emails: Arc<Mutex<Vec<Email>>>,
     fetching_emails: Arc<Mutex<bool>>,
-    viewed_email: Option<Email>,
+    viewed_email: Arc<Mutex<Option<Email>>>,
+    email_count: Arc<Mutex<u32>>,
+    current_offset: Arc<Mutex<u32>>,
 }
 
 impl Default for TmApp {
@@ -32,8 +36,10 @@ impl Default for TmApp {
             domain: "iusearch.lol".into(),
             name,
             emails: Arc::new(Mutex::new(vec![])),
-            viewed_email: None,
+            viewed_email: Default::default(),
             fetching_emails: Default::default(),
+            email_count: Default::default(),
+            current_offset: Default::default(),
         }
     }
 }
@@ -54,30 +60,6 @@ impl TmApp {
         Client::new(self.email()).unwrap()
     }
 
-    fn fetch_emails(&mut self) {
-        let client = self.client();
-        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
-        let status = Arc::clone(&self.fetching_emails);
-        tokio::spawn(async move {
-            *status.lock().unwrap() = true;
-            match client.get_emails(50, 0).await {
-                Ok(emails) => {
-                    let _ = tx.send(emails).await;
-                }
-                Err(err) => {
-                    eprintln!("Failed to fetch emails: {err:?}");
-                }
-            }
-        });
-        let emails_arc = Arc::clone(&self.emails);
-        let status = Arc::clone(&self.fetching_emails);
-        tokio::spawn(async move {
-            while let Some(emails) = rx.recv().await {
-                *emails_arc.lock().unwrap() = emails;
-                *status.lock().unwrap() = false;
-            }
-        });
-    }
     fn top_bar(&mut self, ctx: &Context) {
         TopBottomPanel::top("menubar").show(ctx, |ui| {
             egui::MenuBar::new().ui(ui, |ui| {
@@ -87,7 +69,7 @@ impl TmApp {
                         && ui.button("Delete all emails").clicked()
                     {
                         block_on(self.client().delete_all_emails()).ok();
-                        self.fetch_emails();
+                        self.fetch_emails(false);
                     }
                     if ui.button("Exit").clicked() {
                         ctx.send_viewport_cmd(egui::ViewportCommand::Close);
@@ -102,14 +84,91 @@ impl TmApp {
         S: Into<String>,
     {
         let _ = block_on(self.client().delete_inbox(id.into()));
-        self.fetch_emails();
+        self.fetch_emails(false);
     }
 
-    fn get_email<S>(&self, id: S) -> Email
+    fn fetch_emails(&mut self, extend: bool) {
+        let client = self.client();
+        let mut offset = *self.current_offset.lock().unwrap();
+        if extend {
+            offset += 1;
+        }
+
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let status = Arc::clone(&self.fetching_emails);
+        tokio::spawn(async move {
+            *status.lock().unwrap() = true;
+            match client.get_emails(EMAIL_FETCHING_LIMIT, offset).await {
+                Ok(emails) => {
+                    let _ = tx.send(emails).await;
+                }
+                Err(err) => {
+                    eprintln!("Failed to fetch emails: {err:?}");
+                }
+            }
+        });
+        let emails_arc = Arc::clone(&self.emails);
+        let status = Arc::clone(&self.fetching_emails);
+        tokio::spawn(async move {
+            while let Some(emails) = rx.recv().await {
+                if extend {
+                    emails_arc.lock().unwrap().extend_from_slice(&emails);
+                } else {
+                    *emails_arc.lock().unwrap() = emails;
+                }
+                *status.lock().unwrap() = false;
+            }
+        });
+    }
+    fn count_emails(&mut self) {
+        let client = self.client();
+
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        tokio::spawn(async move {
+            match client.email_count().await {
+                Ok(emails) => {
+                    let _ = tx.send(emails).await;
+                }
+                Err(err) => {
+                    eprintln!("Failed to fetch emails: {err:?}");
+                }
+            }
+        });
+
+        let emails_arc = Arc::clone(&self.email_count);
+        tokio::spawn(async move {
+            while let Some(emails) = rx.recv().await {
+                *emails_arc.lock().unwrap() = emails;
+            }
+        });
+    }
+    fn get_email<S>(&self, id: S)
     where
         S: Into<String>,
     {
-        block_on(self.client().get_inbox(id)).unwrap()
+        let id = id.into();
+        let client = self.client();
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let status = Arc::clone(&self.fetching_emails);
+        tokio::spawn(async move {
+            *status.lock().unwrap() = true;
+            match client.get_inbox(id).await {
+                Ok(emails) => {
+                    let _ = tx.send(emails).await;
+                }
+                Err(err) => {
+                    eprintln!("Failed to fetch emails: {err:?}");
+                }
+            }
+        });
+        let email_arc = Arc::clone(&self.viewed_email);
+        let status = Arc::clone(&self.fetching_emails);
+        tokio::spawn(async move {
+            while let Some(email) = rx.recv().await {
+                *email_arc.lock().unwrap() = Some(email);
+                *status.lock().unwrap() = false;
+            }
+        });
     }
 
     fn email_list(&mut self, _ctx: &Context, ui: &mut egui::Ui) {
@@ -125,16 +184,12 @@ impl TmApp {
             for Email {
                 id,
                 from_address,
-                to_address,
                 subject,
                 received_at,
                 ..
             } in emails.clone()
             {
-                let date = chrono::Local
-                    .timestamp_opt(received_at, 0)
-                    .unwrap()
-                    .to_string();
+                let date = get_date(received_at);
                 Frame::new().stroke(Stroke::default()).show(ui, |ui| {
                     ui.vertical(|ui| {
                         ui.horizontal(|ui| {
@@ -143,20 +198,20 @@ impl TmApp {
                                 self.delete(&id);
                             }
                             if ui.small_button("📩").clicked() {
-                                let email = self.get_email(&id);
-                                self.viewed_email = Some(email);
+                                self.get_email(&id);
                             }
                         });
-                        ui.horizontal(|ui| {
-                            ui.small(from_address);
-                            ui.small("→");
-                            ui.small(to_address);
-                        });
+                        ui.small(from_address);
                         ui.small(date);
                     })
                 });
                 ui.separator();
             }
+            if *self.email_count.lock().unwrap() as usize > emails.len()
+                && ui.button("Load more").clicked()
+            {
+                self.fetch_more_emails();
+            };
         });
     }
 
@@ -178,6 +233,26 @@ impl TmApp {
                 });
         });
     }
+
+    fn fetch_more_emails(&mut self) {
+        self.fetch_emails(true);
+    }
+}
+
+fn get_date(received_at: i64) -> String {
+    let date = chrono::Local.timestamp_opt(received_at, 0).unwrap();
+    let (pm, hour) = date.hour12();
+    let minute = date.minute();
+    let weekday = date.weekday().to_string();
+    let month = Month::try_from(date.month() as u8).unwrap().name();
+    let day = date.day();
+    let year = date.year();
+
+    let date = format!(
+        "{weekday}, {month} {day}, {year} at {hour}:{minute} {}",
+        if pm { "PM" } else { "AM" }
+    );
+    date
 }
 
 impl App for TmApp {
@@ -188,24 +263,29 @@ impl App for TmApp {
 
         CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered_justified(|ui| {
+                ui.set_width(ui.available_width());
+
                 ui.heading(self.email());
                 self.email_edit_view(ui);
 
-                if self.viewed_email.is_some() && ui.button("Back to email list").clicked() {
-                    self.viewed_email = None;
-                }
-                if self.viewed_email.is_none()
+                let viewed_email = self.viewed_email.clone();
+                let mut viewed_email = viewed_email.lock().unwrap();
+
+                if viewed_email.is_some() && ui.button("Back to email list").clicked() {
+                    *viewed_email = None;
+                } else if viewed_email.is_none()
                     && !self.name.is_empty()
                     && ui.button("Fetch emails").clicked()
                 {
-                    self.fetch_emails();
+                    self.fetch_emails(false);
+                    self.count_emails();
                 }
 
                 ui.spacing();
 
                 if *self.fetching_emails.lock().unwrap() {
                     ui.centered_and_justified(|ui| Spinner::new().size(50.).ui(ui));
-                } else if let Some(email) = self.viewed_email.clone() {
+                } else if let Some(email) = viewed_email.clone() {
                     email_view(ui, email);
                 } else {
                     self.email_list(ctx, ui);
@@ -225,20 +305,21 @@ fn email_view(ui: &mut egui::Ui, email: Email) {
         ..
     } = email;
     ScrollArea::vertical().show(ui, |ui| {
-        let date = chrono::Local
-            .timestamp_opt(received_at, 0)
-            .unwrap()
-            .to_string();
+        let date = get_date(received_at);
         ui.vertical(|ui| {
-            ui.heading(format!("subject: {subject}"));
-            ui.heading(format!("time: {date}"));
-            ui.horizontal(|ui| {
-                ui.small(from_address);
-                ui.label("→");
-                ui.small(to_address);
-            });
-            Separator::default().spacing(5.).ui(ui);
-            ui.horizontal_wrapped(|ui| ui.label(text_content.unwrap_or_default()));
+            ui.label(RichText::new(subject).size(40.).strong());
+            Frame::group(ui.style())
+                .inner_margin(Margin::symmetric(16, 16))
+                .corner_radius(ui.style().visuals.menu_corner_radius)
+                .show(ui, |ui| {
+                    ui.set_width(ui.available_width());
+                    ui.label(format!("{:10} {from_address}", "From"));
+                    ui.label(format!("{:10} {to_address}", "To"));
+                    ui.spacing();
+                    ui.small(date);
+                    Separator::default().spacing(20.).ui(ui);
+                    ui.horizontal_wrapped(|ui| ui.label(text_content.unwrap_or_default()));
+                });
         })
     });
 }
